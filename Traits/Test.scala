c13b6cedc1d0fd6861bf91c571e9712718209e92
ajayk1234 <ajaykangare1993@gmail.com>
2018-01-11 19:04:59 +0530

    Traits

---
+++ Traits/Test.scala
@@ -0,0 +1,18 @@
+package Traits
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+
+    def sum(a:Int,s:String,ss:String): Unit ={
+      println(a)
+      println(s)
+    }
+
+    sum(5,"Aaa",ss="aha")
+
+    println(for(i <- 0 to 10) yield 10-i)
+
+  }
+
+}
